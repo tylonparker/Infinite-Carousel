6a37df54af88214f26415339f101c831f1a51628
tylonparker <tylonparker25@gmail.com>
2018-10-07 21:33:44 +0200

    Code Update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Carousel duration={1000}></Carousel>
+         <Carousel duration={1000}></Carousel>
       </div>
     );
   }
